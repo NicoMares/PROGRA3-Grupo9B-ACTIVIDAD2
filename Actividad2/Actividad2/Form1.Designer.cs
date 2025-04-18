8b853a9c4843498fba19e895637594976e407297
BraianUlariaga <ulariagab@gmail.com>
2025-04-17 22:42:50 -0300

    Commit

--- Actividad2/Actividad2/Form1.Designer.cs
+++ Actividad2/Actividad2/Form1.Designer.cs
@@ -31,9 +31,15 @@
             this.dgvArticulos = new System.Windows.Forms.DataGridView();
             this.lblCriterio = new System.Windows.Forms.Label();
             this.lblCampo = new System.Windows.Forms.Label();
+<<<<<<< Updated upstream
             this.lblFiltro = new System.Windows.Forms.Label();
             this.cboCampo = new System.Windows.Forms.ComboBox();
             this.cboCriterio = new System.Windows.Forms.ComboBox();
+=======
+            this.cboCampo = new System.Windows.Forms.ComboBox();
+            this.cboCriterio = new System.Windows.Forms.ComboBox();
+            this.lblFiltro = new System.Windows.Forms.Label();
+>>>>>>> Stashed changes
             this.txtFiltro = new System.Windows.Forms.TextBox();
             this.btnBuscar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
@@ -42,15 +48,25 @@
             // dgvArticulos
             // 
             this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+<<<<<<< Updated upstream
             this.dgvArticulos.Location = new System.Drawing.Point(52, 107);
             this.dgvArticulos.Name = "dgvArticulos";
             this.dgvArticulos.Size = new System.Drawing.Size(414, 283);
+=======
+            this.dgvArticulos.Location = new System.Drawing.Point(34, 116);
+            this.dgvArticulos.Name = "dgvArticulos";
+            this.dgvArticulos.Size = new System.Drawing.Size(536, 298);
+>>>>>>> Stashed changes
             this.dgvArticulos.TabIndex = 0;
             // 
             // lblCriterio
             // 
             this.lblCriterio.AutoSize = true;
+<<<<<<< Updated upstream
             this.lblCriterio.Location = new System.Drawing.Point(60, 38);
+=======
+            this.lblCriterio.Location = new System.Drawing.Point(31, 35);
+>>>>>>> Stashed changes
             this.lblCriterio.Name = "lblCriterio";
             this.lblCriterio.Size = new System.Drawing.Size(39, 13);
             this.lblCriterio.TabIndex = 1;
@@ -59,12 +75,17 @@
             // lblCampo
             // 
             this.lblCampo.AutoSize = true;
+<<<<<<< Updated upstream
             this.lblCampo.Location = new System.Drawing.Point(60, 9);
+=======
+            this.lblCampo.Location = new System.Drawing.Point(31, 8);
+>>>>>>> Stashed changes
             this.lblCampo.Name = "lblCampo";
             this.lblCampo.Size = new System.Drawing.Size(40, 13);
             this.lblCampo.TabIndex = 2;
             this.lblCampo.Text = "Campo";
             // 
+<<<<<<< Updated upstream
             // lblFiltro
             // 
             this.lblFiltro.AutoSize = true;
@@ -75,20 +96,30 @@
             this.lblFiltro.Text = "Filtro";
             this.lblFiltro.Click += new System.EventHandler(this.label3_Click);
             // 
+=======
+>>>>>>> Stashed changes
             // cboCampo
             // 
             this.cboCampo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboCampo.FormattingEnabled = true;
+<<<<<<< Updated upstream
             this.cboCampo.Location = new System.Drawing.Point(113, 6);
             this.cboCampo.Name = "cboCampo";
             this.cboCampo.Size = new System.Drawing.Size(121, 21);
             this.cboCampo.TabIndex = 4;
+=======
+            this.cboCampo.Location = new System.Drawing.Point(81, 5);
+            this.cboCampo.Name = "cboCampo";
+            this.cboCampo.Size = new System.Drawing.Size(121, 21);
+            this.cboCampo.TabIndex = 3;
+>>>>>>> Stashed changes
             this.cboCampo.SelectedIndexChanged += new System.EventHandler(this.cboCampo_SelectedIndexChanged);
             // 
             // cboCriterio
             // 
             this.cboCriterio.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cboCriterio.FormattingEnabled = true;
+<<<<<<< Updated upstream
             this.cboCriterio.Location = new System.Drawing.Point(113, 35);
             this.cboCriterio.Name = "cboCriterio";
             this.cboCriterio.Size = new System.Drawing.Size(121, 21);
@@ -97,13 +128,37 @@
             // txtFiltro
             // 
             this.txtFiltro.Location = new System.Drawing.Point(113, 66);
+=======
+            this.cboCriterio.Location = new System.Drawing.Point(81, 32);
+            this.cboCriterio.Name = "cboCriterio";
+            this.cboCriterio.Size = new System.Drawing.Size(121, 21);
+            this.cboCriterio.TabIndex = 4;
+            this.cboCriterio.SelectedIndexChanged += new System.EventHandler(this.cboCriterio_SelectedIndexChanged);
+            // 
+            // lblFiltro
+            // 
+            this.lblFiltro.AutoSize = true;
+            this.lblFiltro.Location = new System.Drawing.Point(31, 64);
+            this.lblFiltro.Name = "lblFiltro";
+            this.lblFiltro.Size = new System.Drawing.Size(29, 13);
+            this.lblFiltro.TabIndex = 5;
+            this.lblFiltro.Text = "Filtro";
+            // 
+            // txtFiltro
+            // 
+            this.txtFiltro.Location = new System.Drawing.Point(81, 61);
+>>>>>>> Stashed changes
             this.txtFiltro.Name = "txtFiltro";
             this.txtFiltro.Size = new System.Drawing.Size(121, 20);
             this.txtFiltro.TabIndex = 6;
             // 
             // btnBuscar
             // 
+<<<<<<< Updated upstream
             this.btnBuscar.Location = new System.Drawing.Point(270, 38);
+=======
+            this.btnBuscar.Location = new System.Drawing.Point(91, 87);
+>>>>>>> Stashed changes
             this.btnBuscar.Name = "btnBuscar";
             this.btnBuscar.Size = new System.Drawing.Size(75, 23);
             this.btnBuscar.TabIndex = 7;
@@ -114,12 +169,21 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+<<<<<<< Updated upstream
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnBuscar);
             this.Controls.Add(this.txtFiltro);
             this.Controls.Add(this.cboCriterio);
             this.Controls.Add(this.cboCampo);
             this.Controls.Add(this.lblFiltro);
+=======
+            this.ClientSize = new System.Drawing.Size(821, 446);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtFiltro);
+            this.Controls.Add(this.lblFiltro);
+            this.Controls.Add(this.cboCriterio);
+            this.Controls.Add(this.cboCampo);
+>>>>>>> Stashed changes
             this.Controls.Add(this.lblCampo);
             this.Controls.Add(this.lblCriterio);
             this.Controls.Add(this.dgvArticulos);
@@ -137,9 +201,15 @@
         private System.Windows.Forms.DataGridView dgvArticulos;
         private System.Windows.Forms.Label lblCriterio;
         private System.Windows.Forms.Label lblCampo;
+<<<<<<< Updated upstream
         private System.Windows.Forms.Label lblFiltro;
         private System.Windows.Forms.ComboBox cboCampo;
         private System.Windows.Forms.ComboBox cboCriterio;
+=======
+        private System.Windows.Forms.ComboBox cboCampo;
+        private System.Windows.Forms.ComboBox cboCriterio;
+        private System.Windows.Forms.Label lblFiltro;
+>>>>>>> Stashed changes
         private System.Windows.Forms.TextBox txtFiltro;
         private System.Windows.Forms.Button btnBuscar;
     }
